b3b809dccb6c26cc4e4ff07765b61e5113f1a41f
shinhyogeun <tls1gy2rms3@naver.com>
2020-07-26 16:27:19 +0900

    7/26 learning about Constraints

---
+++ AutoLayout/Basic_1_Lecture/Basic_1_Lecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Basic_1_Lecture
+//
+//  Created by 신효근 on 2020/07/26.
+//  Copyright © 2020 신효근. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
