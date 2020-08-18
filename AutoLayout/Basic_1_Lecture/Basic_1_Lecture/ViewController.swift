706a5db4fc6b88245caa7d38264b5911a3bfc4c9
shinhyogeun <tls1gy2rms3@naver.com>
2020-08-18 16:32:47 +0900

    8/18 this update is about Autolayout by code

--- AutoLayout/Basic_1_Lecture/Basic_1_Lecture/ViewController.swift
+++ AutoLayout/Basic_1_Lecture/Basic_1_Lecture/ViewController.swift
@@ -11,9 +11,22 @@ import UIKit
 class ViewController: UIViewController {
 
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let myButtom = UIButton(type: .system)
+        myButtom.setTitle("SYSTEM", for: .normal)
+        
+        self.view.addSubview(myButtom)
+        myButtom.translatesAutoresizingMaskIntoConstraints = false
+        myButtom.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0).isActive = true
+        myButtom.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 0).isActive = true
+        
+        let myLabel = UILabel.init()
+        myLabel.text = "THIS IS MY LABEL"
+        self.view.addSubview(myLabel)
+        myLabel.translatesAutoresizingMaskIntoConstraints = false
+        myLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        myLabel.leftAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leftAnchor, constant: 20).isActive = true
     }
 
 
